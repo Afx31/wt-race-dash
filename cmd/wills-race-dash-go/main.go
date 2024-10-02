3c6df721079a011db79e757231ebeb6c804c8df1
Afx31 <post4wjm@gmail.com>
2024-10-02 21:53:48 +1000

    Setup socketcan package integration

--- cmd/wills-race-dash-go/main.go
+++ cmd/wills-race-dash-go/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "log"
-		"flag"
-    "net/http"
-    
-		"github.com/gorilla/websocket"
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"go.einride.tech/can/pkg/socketcan"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
+const configCanDevice = "vcan0"
+const configStopDataloggingId = uint32(105)
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
+}
+
+type CanData struct {
+	Rpm uint16
+	Speed uint16
+	Gear uint8
+	Voltage uint8
+	Iat uint16
+	Ect uint16
+	Tps uint16
+	Map uint16
+	LambdaRatio uint16
+	OilTemp uint16
+	OilPressure uint16
 }
 
 func handleWs(w http.ResponseWriter, r *http.Request) {
@@ -24,22 +44,54 @@ func handleWs(w http.ResponseWriter, r *http.Request) {
     }
     defer conn.Close()
 
-    for {
-        // Reading message
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("Read error: ", err)
-            return
-        }
-        fmt.Println("Received: ", string(p))
+		// ---------- CANBus data ----------
+		canConn, _ := socketcan.DialContext(context.Background(), "can", configCanDevice)
+		defer canConn.Close()
+		canRecv:= socketcan.NewReceiver(canConn)
 
-        // Writing message back
-        //if err := conn.WriteMessage(websocket.TextMessage, p); err != nil {
-				if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Println("Write error:", err)
-            return
-        }
-    }
+		canData := CanData{}
+
+		for canRecv.Receive() {
+			frame := canRecv.Frame()
+
+			switch frame.ID {
+				case 660:
+				//case 1632:
+					canData.Rpm = binary.BigEndian.Uint16(frame.Data[0:2])
+					canData.Speed = binary.BigEndian.Uint16(frame.Data[2:4])
+					canData.Gear = frame.Data[4]
+					canData.Voltage = frame.Data[5] / 10
+			}
+
+			jsonData, err := json.Marshal(canData)
+			if err != nil {
+				log.Println("Json Marshal: ", err)
+				return
+			}
+
+			if err := conn.WriteMessage(websocket.TextMessage, jsonData); err != nil {
+				fmt.Println("Write error: ", err)
+				return
+			}
+		}
+
+		// ----------------------------------------
+    // for {
+    //     // Reading message
+    //     // messageType, p, err := conn.ReadMessage()
+    //     // if err != nil {
+    //     //     log.Println("Read error: ", err)
+    //     //     return
+    //     // }
+    //     // fmt.Println("Received: ", string(p))
+
+    //     // Writing message back
+		// 		message := []byte("Test message from backend")
+		// 		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+		// 			log.Println("Write error:", err)
+		// 			return
+		// 	}
+    // }
 }
 
 func main() {
