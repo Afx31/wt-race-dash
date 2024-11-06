62edb8c51bdc532b4a1b5e7fbcc621c8dd461955
Afx31 <post4wjm@gmail.com>
2024-11-06 21:11:38 +1100

    Track stats added for SMSP

--- internal/tracks/tracks.go
+++ internal/tracks/tracks.go
@@ -1,19 +1,12 @@
 package tracks
 
-// Left to right readings
-// Lat, Lon
-
-// var MorganPark = [5][2]float64 {
-//   {-28.262071, 152.036342},
-//   {-28.262073, 152.036355},
-//   {-28.262076, 152.036376},
-//   {-28.262079, 152.036399},
-//   {-28.262084, 152.036421},
-// }
-
-var MorganParkLat = [5]float64{-28.262071, -28.262073, -28.262076, -28.262079, -28.262084}
-var MorganParkLon = [5]float64{152.036342, 152.036355, 152.036376, 152.036399, 152.036421}
-var MorganParkLatMin = -28.262071
-var MorganParkLatMax = -28.262084
-var MorganParkLonMin = 152.036342
-var MorganParkLonMax = 152.036421
+var (
+	SMSPLatMin 				= -33.803825
+	SMSPLatMax 				= -33.803653
+	SMSPLonMin 				= 150.870923
+	SMSPLonMax 				= 150.870962
+	MorganParkLatMin 	= -28.262069
+	MorganParkLatMax 	= -28.262085
+	MorganParkLonMin 	= 152.036327
+	MorganParkLonMax 	= 152.036430
+)
