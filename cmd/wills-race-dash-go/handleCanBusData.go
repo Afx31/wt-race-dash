3fa541caecbf0ab6ed748a538f78180dd48b9c8d
Afx31 <post4wjm@gmail.com>
2024-12-07 16:18:47 +1100

    Fixing a few json types, and formatting

--- cmd/wills-race-dash-go/handleCanBusData.go
+++ cmd/wills-race-dash-go/handleCanBusData.go
@@ -46,37 +46,37 @@ type CANFrameMisc struct {
 }
 
 type CANFrame660 struct {
-	Type    int     `json:"Type"`
-	FrameId int			`json:"FrameId"`
-	Rpm     uint16  `json:"Rpm"`
-	Speed   uint16  `json:"Speed"`
-	Gear    uint8   `json:"Gear"`
-	Voltage float32 `json:"Voltage"`
+	Type    	int     `json:"Type"`
+	FrameId 	int			`json:"FrameId"`
+	Rpm     	uint16  `json:"Rpm"`
+	Speed   	uint16  `json:"Speed"`
+	Gear    	uint8   `json:"Gear"`
+	Voltage 	float32 `json:"Voltage"`
 }
 
 type CANFrame661 struct {
-	Type int  `json:"Type"`
-	FrameId int  `json:"Type"`
-	Iat  uint16 `json:"Iat"`
-	Ect  uint16 `json:"Ect"`
+	Type 			int  		`json:"Type"`
+	FrameId 	int  		`json:"FrameId"`
+	Iat  			uint16 	`json:"Iat"`
+	Ect  			uint16 	`json:"Ect"`
 }
 
 type CANFrame662 struct {
-	Type int    `json:"Type"`
-	FrameId int    `json:"Type"`
-	Tps  uint16 `json:"Tps"`
-	Map  uint16 `json:"Map"`
+	Type 			int    	`json:"Type"`
+	FrameId 	int    	`json:"FrameId"`
+	Tps  			uint16 	`json:"Tps"`
+	Map  			uint16 	`json:"Map"`
 }
 
 type CANFrame664 struct {
-	Type        int     `json:"Type"`
-	FrameId        int     `json:"Type"`
-	LambdaRatio float64 `json:"LambdaRatio"`
+	Type        	int     `json:"Type"`
+	FrameId       int     `json:"FrameId"`
+	LambdaRatio 	float64 `json:"LambdaRatio"`
 }
 
 type CANFrame667 struct {
 	Type         int    `json:"Type"`
-	FrameId         int    `json:"FrameId"`
+	FrameId      int    `json:"FrameId"`
 	OilTemp      uint16 `json:"OilTemp"`
 	OilPressure  uint16 `json:"OilPressure"`
 }
